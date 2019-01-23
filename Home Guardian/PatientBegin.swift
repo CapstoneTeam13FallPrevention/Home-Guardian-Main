51ed7c9dca6d5afdbbd7bf2fd68de14f905f75ca
Timothy Dai <ttd004@email.uark.edu>
2019-01-23 11:03:37 -0600

    More Changes to Page Layout.

---
+++ Home Guardian/PatientBegin.swift
@@ -0,0 +1,30 @@
+//
+//  PatientBegin.swift
+//  Home Guardian
+//
+//  Created by Timothy Dai on 1/22/19.
+//  Copyright © 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class PatientBegin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
