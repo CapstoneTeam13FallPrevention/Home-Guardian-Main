1267a1ce9573faffd5f739cf7b9c6139382c7af4
Timothy Dai <ttd004@email.uark.edu>
2019-01-29 17:17:03 -0600

    Picker View Test

--- Home Guardian/PatientBegin.swift
+++ Home Guardian/PatientBegin.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class PatientBegin: UIViewController {
+class PatientBegin: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var clinicTextBox: UITextField!
+    @IBOutlet weak var clinicDropDown: UIPickerView!
+    
+    //Clinic List
+    var clinicList = ["Mercy","Sparks"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        return clinicList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        self.view.endEditing(true)
+        return clinicList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        self.clinicTextBox.text = self.clinicList[row]
+        self.clinicDropDown.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        if textField == self.clinicTextBox {
+            self.clinicDropDown.isHidden = false
+            //if you don't want the users to se the keyboard type:
+            
+            textField.endEditing(true)
+        }
+    }
     /*
     // MARK: - Navigation
 
