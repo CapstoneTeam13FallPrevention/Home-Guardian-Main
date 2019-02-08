69ed279b50a4ed5350ec486ef37670d240f4b909
Timothy Dai <ttd004@email.uark.edu>
2019-02-08 09:32:31 -0600

    Attempt of User Agreement Scroll.

---
+++ Home Guardian/UserAgreementControl.swift
@@ -0,0 +1,18 @@
+//
+//  UserAgreementControl.swift
+//  Home Guardian
+//
+//  Created by Timothy Dai on 2/8/19.
+//  Copyright © 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class UserAgreementControl: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
