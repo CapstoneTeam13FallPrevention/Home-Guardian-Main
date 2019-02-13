5839ba09ba8d45639c1859e5d7602a464cec87ef
Timothy Dai <ttd004@email.uark.edu>
2019-02-13 10:12:27 -0600

    Camera Test.

--- Home Guardian/CameraController.swift
+++ Home Guardian/CameraController.swift
@@ -8,10 +8,44 @@
 
 import UIKit
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var userImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func cameraAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            
+            imagePicker.allowsEditing = false
+            
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            self.present(imagePicker, animated: false, completion: nil)
+        } else {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            
+            imagePicker.allowsEditing = false
+            
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(imagePicker, animated: false, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            userImage.image = image
+        } else {
+            print("Error!")
+        }
+        
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    
 }
