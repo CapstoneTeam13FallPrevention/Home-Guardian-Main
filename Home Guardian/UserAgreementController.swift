5d0665e36f914dd8c0b9ddbbc4bebe1578ce0fe7
Timothy Dai <ttd004@email.uark.edu>
2019-02-20 11:22:13 -0600

    Pop Up Section Test.

--- Home Guardian/UserAgreementController.swift
+++ Home Guardian/UserAgreementController.swift
@@ -16,23 +16,46 @@ class UserAgreementController: UIViewController {
     }
     
     @IBAction func userAgreementPopUp(_ sender: Any) {
-         let actionSheet = UIAlertController(title: "User Agreement", message: "Choose part to view", preferredStyle: . actionSheet)
-         
-         actionSheet.addAction(UIAlertAction(title: "Terms and Conditions", style: .default, handler: { (action:UIAlertAction) in
-         
-         }))
-         
-         actionSheet.addAction(UIAlertAction(title: "Privacy Policy", style: .default, handler: { (action:UIAlertAction) in
-
-         }))
-         
-         actionSheet.addAction(UIAlertAction(title: "Disclaimer/Liability", style: .default, handler: { (action:UIAlertAction) in
-
-         }))
-         
-         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
-         
-         self.present(actionSheet, animated: true, completion: nil)
+        let actionSheet = UIAlertController(title: "User Agreement", message: "Choose part to view", preferredStyle: . actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Terms and Conditions", style: .default, handler: { (action:UIAlertAction) in
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "tac") as! UserAgreementSections
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Privacy Policy Part 1", style: .default, handler: { (action:UIAlertAction) in
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "pp1") as! UserAgreementSections
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Privacy Policy Part 2", style: .default, handler: { (action:UIAlertAction) in
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "pp2") as! UserAgreementSections
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Privacy Policy Part 3", style: .default, handler: { (action:UIAlertAction) in
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "pp3") as! UserAgreementSections
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Disclaimer/Liability", style: .default, handler: { (action:UIAlertAction) in
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "dsc") as! UserAgreementSections
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
     }
     
 }
---
+++ Home Guardian/UserAgreementSections.swift
@@ -0,0 +1,36 @@
+//
+//  UserAgreementSections.swift
+//  Home Guardian
+//
+//  Created by Timothy Dai on 2/20/19.
+//  Copyright © 2019 Capstone. All rights reserved.
+//
+
+import UIKit
+
+class UserAgreementSections: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func tacBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pp1Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func pp2Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func pp3Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func dscBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
